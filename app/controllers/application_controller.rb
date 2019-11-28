aa2afd894efb7a3e2a0f1e99a8b488db40b2cf0f
kakiharahiroaki <kaki.hiro.88888@gmail.com>
2019-11-28 10:32:51 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  def after_sign_up_path_for(resource)
+    user_path_url(resource)
+  end
+
+
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i(name self_introduction select img))
+    devise_parameter_sanitizer.permit(:account_update, keys: %i(name self_introduction select img))
   end
 end
