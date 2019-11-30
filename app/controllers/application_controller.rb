a06970d8271b2ded154a277bcaeaa44ccfa1799d
kakiharahiroaki <kaki.hiro.88888@gmail.com>
2019-11-30 15:47:37 +0900

    users一覧ページの作成・log_inページからのリンク付け

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
-  def after_sign_up_path_for(resource)
-    user_path_url(resource)
-  end
-
-
+  
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i(name self_introduction select img))
     devise_parameter_sanitizer.permit(:account_update, keys: %i(name self_introduction select img))
+  end
+
+  def after_sign_in_path_for(resource)
+    users_path
+  end
+  def after_sign_up_path_for(resource)
+    users_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id:params[:id])
   end
-
+    
   def index
+    @user = User.where.not(id:current_user.id)
   end
 end
