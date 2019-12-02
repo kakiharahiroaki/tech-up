933d6e0706129e20a553c94dd3deed4a0a7d993c
kakiharahiroaki <kaki.hiro.88888@gmail.com>
2019-12-02 19:15:59 +0900

    user編集ページ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,7 @@ class ApplicationController < ActionController::Base
   def after_sign_up_path_for(resource)
     users_path
   end
+  def after_user_registration_path_for(resource)
+    users_path
+  end
 end
